e2f2af50fa10ee3d5d657b944f420cc3e1aa39ba
unknown <2273359360@qq.com>
2021-12-21 23:07:59 +0800

    12.21

---
+++ src/String/1154-DayoftheYear/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution1154
+{
+private:
+  // 每个月有多少天， 0 - 11月
+  const int presum[12] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+
+public:
+  int dayOfYear(string date)
+  {
+    // 获取年月日
+    int year = stoi(date.substr(0, 4));
+    int month = stoi(date.substr(5, 2));
+    int day = stoi(date.substr(8, 2));
+
+    // 计算今天是当年的第几天
+    int res = 0;
+    res += presum[month - 1];
+    // 是闰年的话需要额外加一天，前提是当前月份是超过 2 月的
+    if (month > 2)
+      if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+        res += 1;
+    res += day;
+
+    return res;
+  }
+};
+
+int main(void)
+{
+
+  return 0;
+}
