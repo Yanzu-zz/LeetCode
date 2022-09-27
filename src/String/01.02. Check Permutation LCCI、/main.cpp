64894b206149d9d7782fc85b927957e80f48e9f1
Yanzu-zz <2273359360@qq.com>
2022-09-27 22:07:18 +0800

    09.27

---
+++ src/String/01.02. Check Permutation LCCI、/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+  bool CheckPermutation(string s1, string s2)
+  {
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+
+    return s1 == s2;
+  }
+};
+
+int main(void)
+{
+
+  return 0;
+}
